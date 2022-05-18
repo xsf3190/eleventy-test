b5ce22131b5b1bb2eb7039ee600c74f0e71d919d
Mark Brown <mark.russellbrown@gmail.com>
2022-05-18 09:18:21 +0100

    functions

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,13 @@ module.exports = function(config) {
 
     config.addPassthroughCopy("src/js");
 
+    config.setBrowserSyncConfig({
+        https: {
+            key: '/Users/markrussel-brown/Documents/mrbapex/eleventy-test/localhost.key',
+            cert: '/Users/markrussel-brown/Documents/mrbapex/eleventy-test/localhost.crt'
+        }
+    })
+
     return {
         dir: {
             input: "src",
